e8d0ddb59580b1a648a7ed1a2afd00fa5a28b063
matt <choim3@spu.edu>
2021-05-28 11:19:58 -0700

    grid practice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import "./app.css";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <div className="box1">1</div>
+      <div className="box2">2</div>
+      <div className="box3">3</div>
+      <div className="box4">4</div>
+      <div className="box5">5</div>
+    </div>
+  );
 }
 
 export default App;
